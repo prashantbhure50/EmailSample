3ece8b1b8f68c0db33f267de96102c11c4c12d8e
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-23 20:38:10 +0530

    Add project files.

---
+++ EmailSample/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailSample
+{
+    class Pattern
+    {
+        string regex = "^[a-z0-9A-Z]+([.#@_][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+
+        public bool Check(string input)
+        {
+            return Regex.IsMatch(input, regex);
+        }
+    }
+}
